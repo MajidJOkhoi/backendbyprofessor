0586754d3bc34426af8fc0d7b3f7c39dbd5ae7bd
majid ali <majidalijkhio@gmail.com>
2024-07-14 21:47:28 +0500

    api error hanlder

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const path = require("path");
+
+// Load environment variables from .env file
+dotenv.config();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    exposedHeaders: ["Set-Cookie"],
+  })
+);
+
+// Middleware to parse cookies from the request headers
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static(path.join(__dirname, "public")));
+// Middleware to parse JSON request bodies
+app.use(express.json({ limit: "20kb" }));
 
 
 
